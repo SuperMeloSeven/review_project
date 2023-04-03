e9626e843db467b24f056dbcb78f28761472ac14
huzhilei <huzhilei@delonix.group>
2023-04-03 15:27:35 +0800

    feat(*): 单例模式-全局模态框实现

--- javascript/14-单例模式.js
+++ javascript/14-单例模式.js
@@ -1,14 +1,37 @@
-class Single {
-  show() {
-    console.log('show');
-  }
-  static getInstance() {
-    if (!Single.instance) {
-      Single.instance = new Single()
-    }
-  }
-}
+// class Single {
+//   show() {
+//     console.log('show');
+//   }
+//   static getInstance() {
+//     if (!Single.instance) {
+//       Single.instance = new Single()
+//     }
+//   }
+// }
 
-const s1 = Single.getInstance()
-const s2 = Single.getInstance()
-console.log(s1 === s2, '---s1 === s2');
+// const s1 = Single.getInstance()
+// const s2 = Single.getInstance()
+// console.log(s1 === s2, '---s1 === s2');
+
+const Modal = (function () {
+  let modal = null
+  return function () {
+    if (!modal) {
+      modal = document.createElement('div')
+      modal.innerHTML = '我是全局模态框'
+      modal.id = 'modal'
+      modal.style.display = 'none'
+      document.body.appendChild(modal)
+    }
+    return modal
+  }
+})()
+
+document.getElementById('open').addEventListener('click', function () {
+  const modal = new Modal()
+  modal.style.display = 'block'
+})
+document.getElementById('close').addEventListener('click', function () {
+  const modal = new Modal()
+  modal && (modal.style.display = 'none')
+})
